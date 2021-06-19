18b3e9b263265a22daa1da3f4382f939da877705
shugonaga <75158141+shugonaga@users.noreply.github.com>
2021-06-19 10:34:04 +0900

    ファイル

---
+++ tech6.js
@@ -0,0 +1,165 @@
+
+document.getElementById("button").onclick = function () {
+
+    //クリックしてスタート
+    //facebook-jsonを取得して表示
+    let count = 0;
+    const limit = 50; //表示件数
+    const graph_api = 'https://graph.facebook.com/ig_hashtag_search?';
+    const accessToken = 'EAAnGQ8kLkaUBAA7uwcktBemDqtU819jSBOkvozeWqzAlGorGFTu1HEKJK2xnnTFJSEXDEcZCUn6xpjMc41HNYohEusQ3uMLyBZBhZANhE8Gk2JrTBzwPiOkwpHsZCJZC4OZBoLaDJBTxFz5aqKSZC2ZAUIuVvhZBI4VakZAOQsyVgSnX7pZBfhsUpZBo168dtdpSHocZD'; 
+    const businessID = "17841441477914224"; //グラフAPIエクスプローラで取得したinstagram_business_accountのID
+    let text = '';
+    let hashtag = [];
+    let input_message = [];
+    input_message = document.getElementById("input_message").value; //テキストボックス内のキーワードを格納
+    // console.log(input_message[5]);
+    hashtag = input_message.split(/\s+/);
+    console.log(hashtag);
+    console.log(hashtag.length);
+    // console.log(hashtag[0]);
+
+
+    //promise.allを使って投稿検索を非同期処理する必要！
+    // 非同期処理の定義
+
+
+    var dataMedias = {};
+    function func1(t) {
+        console.log(t);
+
+        return new Promise(function (resolve, reject) {
+
+            const url0 = graph_api + "user_id=" + businessID + "&q=" + t + "&access_token=" + accessToken;
+            var dataId;
+            var dataMedia;
+            //タグID検索
+            fetch(url0)
+                .then((response) => {
+                    return response.json() //ここでBodyからJSONを返す
+
+                })
+                .then((result) => {
+                    dataId = result;
+
+                    const url1 = 'https://graph.facebook.com/'
+                    const url2 = '/recent_media?user_id=' + businessID + '&fields=media_url,permalink&limit=' + limit + '&access_token=' + accessToken;
+                    const url3 = url1 + dataId.data[0].id + url2;
+
+                    //console.log(url3);
+
+                    fetch(url3)
+                        .then((response) => {
+                            return response.json()
+                        })
+
+                        .then((result) => {
+                            console.log(t + "first");
+                            //console.log(result);
+                            dataMedias[t] = result;
+
+                            //func2(dataMedia);
+                            // for (let i = 0; i < 10; i++) {
+                            //     text1 = '<li><a href="' + dataMedia.data[i].permalink + '" target="_blank">';
+                            //     text2 = '<img src="' + dataMedia.data[i].media_url + '">';
+                            //     text3 = '</a></li>';
+                            //     text = text + text1 + text2 + text3;
+                            // }
+
+                            // $('#instagram-list').html(text);
+
+
+                            //after
+                            let after = dataMedias[t].paging.cursors.after;                            
+
+                            async function myasync(after2) {
+                                // console.log('myasync');
+                                const url4 = url1 + dataId.data[0].id + '/recent_media?user_id=' + businessID + '&fields=media_url,permalink&limit=' + limit + '&after=' + after2 + '&access_token=' + accessToken;
+                                await fetch(url4)
+                                    .then((response) => {
+                                        return response.json()
+                                    })
+
+                                    .then((result) => {
+                                        console.log(t + "after")
+                                        //console.log(result);
+                                        const sampleReslt = {
+                                            "福島":[
+                                                {},{},{}
+                                            ],
+                                            "温泉":[
+                                                {},{},{}
+                                            ]
+                                        };
+                                        const oldResult = [
+                                            [{},{},{}],
+                                            [{},{},{}]
+                                        ];
+                                        dataMedia2 = result;
+                                        Array.prototype.push.apply(dataMedias[t].data, result.data);
+                                        console.log(dataMedias[t]);
+
+                                        after = dataMedia2.paging.cursors.after;
+                                        // //  console.log(after);
+                                        // url4 = url1 + dataId.data[0].id + '/recent_media?user_id=' + businessID + '&fields=media_url,permalink&limit=' + limit + '&after=' + after + '&access_token=' + accessToken;
+                                        // for (let i = 0; i < 50; i++) {
+                                        //     // console.log(i);
+                                        //     text1 = '<li><a href="' + dataMedia2.data[i].permalink + '" target="_blank">';
+                                        //     text2 = '<img src="' + dataMedia2.data[i].media_url + '">';
+                                        //     text3 = '</a></li>';
+                                        //     text = text + text1 + text2 + text3;
+                                        // }
+
+                                        $('#instagram-list').html(text);
+
+                                        if (dataMedias[t].data.length < 200) {
+                                            myasync(after);
+                                        }
+                                        return;
+                                    })
+                                    .catch((e) => {
+                                        console.log(e) //エラーをキャッチし表示     
+                                    })
+                            }
+                            myasync(after);
+                        })
+                        .catch((e) => {
+                            console.log(e) //エラーをキャッチし表示     
+                        })
+                })
+                .catch((e) => {
+                    console.log(e) //エラーをキャッチし表示     
+                })
+        })
+    }
+
+    // function func2 (mmm) {
+    //     console.log(mmm);
+    // }
+
+    if (hashtag.length < 5) {
+        Promise.all(hashtag.map(function (tag) {
+                // console.log(tag);
+                // return func1(tag);
+                return new Promise(resolve => {
+                    return func1(tag)
+                        .then(result => {
+                            resolve(result);
+                        });
+                    // setTimeout(() => {
+                    //   console.log(new Date());
+                    //   console.log(tag);
+                    //   resolve(tag);
+                    // },3000)
+                })
+
+            }))
+            .then((result) => {
+                console.log(result);
+            })
+            .catch((result) => {
+                console.log(result);
+            });
+    }else{
+        console.log("五つ以下のタグを入力してください");
+    }
+}
---
+++ tech7.js
@@ -0,0 +1,94 @@
+document.getElementById("button").onclick = function () {  //クリックしてスタート
+
+    //初期値設定
+    const limit = 50; //表示件数
+    const graph_api = 'https://graph.facebook.com/ig_hashtag_search?';
+    const accessToken = 'EAAnGQ8kLkaUBAGFZBOS9ZBMPQWgZBvd5SdUppEXiHTW0GbrBP7xkSdU4bV4y7zVoDblKy8GMDBwSw1tZA9WMfaYZCwvIsnYZBwAi7s7PmbZCD9pCARGQhb8YlaQZAmnthpnzu4sMEEqZCtK2vV1zafoIWjcEgEOlJqZA1qMswJ85162aS9iVbSwCpF'; // アクセストークン
+    const businessID = '17841441477914224'; //グラフAPIエクスプローラで取得したinstagram_business_accountのID
+    let text = ''; //表示処理の際利用
+    let hashtag = [];
+    let input_message = [];
+    let dataMedias = {};
+    let dataId;
+    input_message = document.getElementById("input_message").value; //テキストボックス内のキーワードを格納
+    hashtag = input_message.split(/\s+/);
+    console.log(hashtag);
+    //console.log(hashtag.length);
+
+    //メイン処理
+    function func1(t) {
+        return new Promise(function (resolve, reject) {
+            dataMedias[t]=[];　　//tをキーに配列を作成
+            const url0 = graph_api + "user_id=" + businessID + "&q=" + t + "&access_token=" + accessToken;
+             //タグID検索
+             fetch(url0)
+                .then((response) => {
+                    return response.json() //ここでBodyからJSONを返す,定型処理
+                 })
+                .then((result) => {
+                    dataId = result; //JSONをdataIdに代入
+                    const url1 = 'https://graph.facebook.com/'
+                    const url2 = '/recent_media?user_id=' + businessID + '&fields=media_url,permalink&limit=' + limit + '&access_token=' + accessToken;
+                    const url3 = url1 + dataId.data[0].id + url2;//投稿検索URLの作成
+
+                    fetch(url3) //投稿検索URLの展開
+                        .then((response) => {
+                            return response.json()//JSONを返す
+                        })
+                        .then((result) => {
+                            dataMedias[t] = result.data;//配列にデータを格納
+
+                            //after処理
+                            let after = result.paging.cursors.after;                            
+                            async function myasync(after2) {
+                                const url4 = url1 + dataId.data[0].id + '/recent_media?user_id=' + businessID + '&fields=media_url,permalink&limit=' + limit + '&after=' + after2 + '&access_token=' + accessToken;
+                                await fetch(url4)
+                                    .then((response) => {
+                                        return response.json()
+                                    })
+                                    .then((result) => {
+                                        //console.log(t + "after")
+                                        dataMedia2 = result.data;
+                                        dataMedias[t] = dataMedias[t].concat(result.data);   //afterデータの追加
+
+                                        after = result.paging.cursors.after;  //次のafterの取得
+                                        if (dataMedias[t].length < 90) {
+                                            myasync(after);
+                                        }else{
+                                            console.log(dataMedias);
+                                            console.log(t);
+                                            // let key = t;
+
+                                            //dataMedias[t]中の任意の値を取り出す
+                                            console.log(JSON.stringify(Object.values(dataMedias[t])[10].id));
+
+                                            //console.log(Object.values(dataMedias[t])[0]);　　
+                                            //console.log(Object.values(dataMedias[t]));　
+                                            //console.log(JSON.stringify(dataMedias[t]));　 
+                                        }
+                                        return;
+                                    })
+                            }
+                            myasync(after);
+                        })
+                })
+        })
+    }
+
+
+    //開始位置
+    if (hashtag.length < 5) {
+        Promise.all(hashtag.map(function (tag) {
+            return new Promise(resolve => {
+                return func1(tag)
+                    .then(result => {
+                        resolve(result);
+                    });
+            });
+        }));
+        
+    }else{
+        console.log("五つ以下のタグを入力してください");
+    }
+            
+}
